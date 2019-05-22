18b71456ba1fc123b0784e183ee37bd6bedc097c
GEORGE Amos <amos.george@telecomnancy.eu>
2019-05-22 13:24:31 +0200

    fin du debug

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,6 @@ public final class Main {
 	public static final void main (String [] arguments) throws LWJGLException {
 		String title = "Sans titre";
 		
-        int bpp = 24;
-        // get the resolutions and frequencys from XRandR
-        System.err.println("xrandr " + XRandR.getScreenNames().length);
-        Screen[] resolutions = XRandR.getResolutions(XRandR.getScreenNames()[0]);
-        System.err.println("resolutions " + resolutions.length);
-        DisplayMode[] modes = new DisplayMode[resolutions.length];
-        System.err.println("modes " + modes.length);
-		
-		System.err.println();
-		
 		new AppGame (title, 1280, 1024, true) {
 			@Override
 			public void init () {
